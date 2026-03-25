3f03c965ac42927ac30d687a88c3694c47aaa5f2
natashasantosf <natasha.s.felipe@gmail.com>
2026-03-25 15:13:48 -0300

    Fix DATABASE_URL fallback

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./todo.db")
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
